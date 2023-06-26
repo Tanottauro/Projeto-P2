ab79f9ede00bd55d87f9395c6ee27a9c1b43f4b2
Gabriel Raposo <gabriel.henrique7@hotmail.com>
2023-06-26 05:25:43 -0300

    Alterações
    Mano, fiz muita coisa, resumindo, coloquei funcionalidade na criação de itens, fiz o consumo comer a quantidade de itens  a cada 3 segundos, para ver em tempo real.
    Adicionei funcionalidade mas não completamente aos botões, laterais.

--- script.js
+++ script.js
@@ -19,3 +19,147 @@ function closebarralateral() {
 
 
 
+
+function adicionarItem() {
+
+  const categoriaProduto = document.getElementById('categoriaProduto').value;
+
+  const nomeProduto = document.getElementById('nomeProduto').value;
+
+  const dataValidade = document.getElementById('dataValidade').value;
+
+  const quantProduto = document.getElementById('quantProduto').value;
+
+  const medConsumoProduto = document.getElementById('medConsumoProduto').value;
+
+  
+
+  const item = document.createElement('li');
+
+  
+
+  const categoriaSpan = document.createElement('span');
+
+  categoriaSpan.textContent = `Categoria: `;
+
+  const categoriaStrong = document.createElement('strong');
+
+  categoriaStrong.textContent = categoriaProduto;
+
+  categoriaSpan.appendChild(categoriaStrong);
+
+  item.appendChild(categoriaSpan);
+
+  
+
+  item.appendChild(document.createElement('br'));
+
+  
+
+  const nomeSpan = document.createElement('span');
+
+  nomeSpan.textContent = `Nome: `;
+
+  const nomeStrong = document.createElement('strong');
+
+  nomeStrong.textContent = nomeProduto;
+
+  nomeSpan.appendChild(nomeStrong);
+
+  item.appendChild(nomeSpan);
+
+  
+
+  item.appendChild(document.createElement('br'));
+
+  
+
+  const validadeSpan = document.createElement('span');
+
+  validadeSpan.textContent = `Validade: `;
+
+  const validadeStrong = document.createElement('strong');
+
+  validadeStrong.textContent = dataValidade;
+
+  validadeSpan.appendChild(validadeStrong);
+
+  item.appendChild(validadeSpan);
+
+  
+
+  item.appendChild(document.createElement('br'));
+
+  
+
+  const quantSpan = document.createElement('span');
+
+  quantSpan.textContent = `Quantidade em estoque: `;
+
+  const quantStrong = document.createElement('strong');
+
+  quantStrong.setAttribute('id', `quant-${nomeProduto}`); // Atributo de ID para acessar o elemento da quantidade em estoque
+
+  quantStrong.textContent = quantProduto;
+
+  quantSpan.appendChild(quantStrong);
+
+  item.appendChild(quantSpan);
+
+  
+
+  item.appendChild(document.createElement('br'));
+
+  
+
+  const consumoSpan = document.createElement('span');
+
+  consumoSpan.textContent = `Consumo diário: `;
+
+  const consumoStrong = document.createElement('strong');
+
+  consumoStrong.textContent = medConsumoProduto;
+
+  consumoSpan.appendChild(consumoStrong);
+
+  item.appendChild(consumoSpan);
+
+  
+
+  document.getElementById('listaItens').appendChild(item);
+
+  
+
+  document.getElementById('id01').style.display='none';
+
+  
+
+  // Limpar campos do formulário
+
+  document.getElementById('categoriaProduto').value = '';
+
+  document.getElementById('nomeProduto').value = '';
+
+  document.getElementById('dataValidade').value = '';
+
+  document.getElementById('quantProduto').value = '';
+
+  document.getElementById('medConsumoProduto').value = '';
+
+  
+
+  // Diminuir quantidade em estoque a cada minuto
+
+  setInterval(() => {
+
+    const quantElement = document.getElementById(`quant-${nomeProduto}`);
+
+    let quantAtual = Number(quantElement.textContent);
+
+    quantAtual -= Number(medConsumoProduto);
+
+    quantElement.textContent = String(quantAtual);
+
+  }, 3000); // 3000 ms = 3 segundos
+
+}
